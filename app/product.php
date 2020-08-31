1ed7b6d48a7f9e638a5f49226a32d9cde41b7c5d
stephenkurnia09 <stevekurnia06@gmail.com>
2020-08-31 20:16:18 +0700

    change_1

--- app/product.php
+++ app/product.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
 {
-    protected $fillable = [
-        'name', 'description', 'type', 'price', 'stock'
-    ];
+    protected $guarded = [];
 }
